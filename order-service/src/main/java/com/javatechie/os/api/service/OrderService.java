4369d06ab365b4b78c4d63ba05d9b62b9a84ebc0
Admin <nguyenvantuyen6789@gmail.com>
2021-08-28 12:39:56 +0700

    Gọi từ Order Service sang Payment Service dùng PAYMENT-SERVICE ở OrderService.java

--- order-service/src/main/java/com/javatechie/os/api/service/OrderService.java
+++ order-service/src/main/java/com/javatechie/os/api/service/OrderService.java
@@ -30,7 +30,7 @@ public class OrderService {
 
         System.out.println("hh1");
         System.out.println(payment);
-        Payment paymentResponse = template.postForObject("http://localhost:8081/payment/doPayment", payment, Payment.class);
+        Payment paymentResponse = template.postForObject("http://PAYMENT-SERVICE/payment/doPayment", payment, Payment.class);
 
         System.out.println("hh2");
         response = paymentResponse.getPaymentStatus().equals("success") ? "payment processing successfully and order placed" : "there is a failure in payment api, order added to cart";
