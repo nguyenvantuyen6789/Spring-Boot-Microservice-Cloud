ac94e031f26b7da6ea352a8a2c9a0405bf0c92f6
Admin <nguyenvantuyen6789@gmail.com>
2021-08-28 10:54:21 +0700

    Tạo Service Registry. Tạo Cloud và name cho các service như Order Service, Payment Service

--- order-service/src/main/java/com/javatechie/os/api/service/OrderService.java
+++ order-service/src/main/java/com/javatechie/os/api/service/OrderService.java
@@ -28,7 +28,7 @@ public class OrderService {
         payment.setOrderId(order.getId());
         payment.setAmount(order.getPrice());
 
-        Payment paymentResponse = template.postForObject("http://localhost:8081/payment/doPayment", payment, Payment.class);
+        Payment paymentResponse = template.postForObject("http://PAYMENT-SERVICE/payment/doPayment", payment, Payment.class);
 
         response = paymentResponse.getPaymentStatus().equals("success") ? "payment processing successfully and order placed" : "there is a failure in payment api, order added to cart";
 
